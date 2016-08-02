381c4a3c2927396bb58d111aa3063bad4b9d4412
ihiteshri12 <hiti.ramteke93@gmail.com>
2016-08-02 10:29:53 +0000

    correct details filled

--- apache2/metadata.rb
+++ apache2/metadata.rb
@@ -1,6 +1,6 @@
 name             'apache2'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'opex software'
+maintainer_email 'hiteshri.ramteke@opexsoftware.com'
 license          'All rights reserved'
 description      'Installs/Configures apache2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
